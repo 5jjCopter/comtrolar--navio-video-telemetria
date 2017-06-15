9c07449a63255db24b6be720a7a4a92678ee5e39
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-06-15 14:11:28 +0200

    Create permisos.sh

---
+++ permisos.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /home/pi
+cd comtrolar--navio-video-telemetria-master
+sudo chmod 775 graba-video-1080-30f
+sudo chmod 775 graba-video-750-30f
+sudo chmod 775 video-1080
+sudo chmod 775 video-720
+sudo chmod 775 temperatura
