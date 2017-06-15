98112901ceacf6386810c4f46c683d838df74cc8
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-06-15 13:24:16 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo wget https://github.com/5jjCopter/comtrolar--navio-video-telemetria/archive/master.zip
+sudo unzip master.zip
+sudo rm master.zip
+cd home/pi/comtrolar--navio-video-telemetria
+
