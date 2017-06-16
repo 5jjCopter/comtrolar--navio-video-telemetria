81d07573ba13cbb87a9e3dfe8dce5c4dc3267306
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-06-16 09:10:56 +0200

    Update permisos.sh

--- permisos.sh
+++ permisos.sh
@@ -2,7 +2,12 @@
 cd /home/pi
 cd comtrolar--navio-video-telemetria-master
 sudo chmod 775 graba-video-1080-30f
-sudo chmod 775 graba-video-750-30f
+sudo chmod 775 graba-video-720-30f
 sudo chmod 775 video-1080
 sudo chmod 775 video-720
 sudo chmod 775 temperatura
+sudo chmod 775 install-systemctl
+sudo chmod 775 interfaces
+sudo chmod 775 install-solo-video
+sudo chmod 775 monta-automatica-pendrive
+sudo chmod 775 actualizacion-imagen
