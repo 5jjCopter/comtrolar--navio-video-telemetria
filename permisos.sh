f80cb4d8e6b1434893a99bda81e4f556332a352d
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-06-16 09:18:35 +0200

    Update permisos.sh

--- permisos.sh
+++ permisos.sh
@@ -5,7 +5,7 @@ sudo chmod 775 graba-video-1080-30f
 sudo chmod 775 graba-video-720-30f
 sudo chmod 775 video-1080
 sudo chmod 775 video-720
-sudo chmod 775 temperatura
+sudo chmod 775 temperatura.py
 sudo chmod 775 install-systemctl
 sudo chmod 775 interfaces
 sudo chmod 775 install-solo-video
