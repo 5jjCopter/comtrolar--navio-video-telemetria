b040d702de3771a94b19c36cc7b9a9876cf51f20
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-06-16 12:13:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,35 @@ cd comtrolar--navio-video-telemetria-master
 sudo chmod 775 permisos.sh
 sudo ./permisos.sh
 sudo ./install-systemctl
-sudo ./install-ip-fija
+cd /home/pi
+
+sudo wget https://github.com/5jjCopter/navio/archive/master.zip
+
+sudo unzip master.zip
+sudo rm master.zip
+cd
+
+cd /etc/network
+
+sudo rm interfaces
+
+cd /etc/wpa_supplicant
+
+sudo rm wpa_supplicant.conf
+cd
+cd navio-master
+
+sudo chmod 775 interfaces
+
+sudo chmod 775 wpa_supplicant.conf
+
+sudo cp interfaces /etc/network
+
+sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+cd
+
+sudo rm -rf navio-master
+cd comtrolar--navio-video-telemetria-master
 sudo ./actualizacion-imagen
 sudo reboot
