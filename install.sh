ecef75ee5df3045a6b7405a8b080b13b9b5449c7
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-06-16 08:59:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo wget https://github.com/5jjCopter/comtrolar--navio-video-telemetria/archive/master.zip
 sudo unzip master.zip
 sudo rm master.zip
-cd home/pi/comtrolar--navio-video-telemetria
+cd home/pi/comtrolar--navio-video-telemetria-master
 sudo ./permisos.sh
 sudo ./install-systemctl
 sudo ./interfaces
