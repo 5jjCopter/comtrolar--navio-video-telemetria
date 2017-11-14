267302843f527df0819c6a2968fb7a6661263510
juanjojjjjjj <juanjojjjjjj@gmail.com>
2017-11-14 10:35:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
+cd
 sudo wget https://github.com/5jjCopter/comtrolar--navio-video-telemetria/archive/master.zip
 sudo unzip master.zip
 sudo rm master.zip
 cd
 cd comtrolar--navio-video-telemetria-master
 sudo chmod 775 permisos.sh
+sudo chmod 775 install-systemctl
+sudo chmod 775 monta-automatica-pendrive
 sudo ./permisos.sh
 sudo ./install-systemctl
+sudo cp monta-automatica-pendrive /home/pi
+cd
+
+sudo ./monta-automatica-pendrive
 cd /home/pi
+
+sudo rm -rf monta-automatica-pendrive-master
 
 sudo wget https://github.com/5jjCopter/navio/archive/master.zip
 
